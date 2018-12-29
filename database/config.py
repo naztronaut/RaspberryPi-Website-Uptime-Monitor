83cdd43984807442d06faca874b0dfb34cc5b014
Nazmus <nazmus.nasir@gmail.com>
2018-12-29 02:31:59 -0500

    Create config.py
    database values

---
+++ database/config.py
@@ -0,0 +1,9 @@
+# config.py
+# database values
+
+DATABASE_CONFIG = {
+    'host' : 'localhost',
+    'dbname' : 'uptime',
+    'dbuser' : 'ENTER_DATABSE_USERNAME',
+    'dbpass' : 'ENTER_DATABASE_PASSWORD'
+}
