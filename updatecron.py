56b7438689041bf26890a56d3fa65b7dd78c18f4
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-01-01 23:39:55 -0500

    updated cron tasks

--- updatecron.py
+++ updatecron.py
@@ -2,7 +2,7 @@ from crontab import CronTab
 
 user_cron = CronTab(user='pi')
 
-job = user_cron.new(command='python3 checkSitesCron.py', comment='checksites')
+job = user_cron.new(command='python3 /home/pi/uptime/checkSitesCron.py', comment='checksites')
 
 job.minute.every(10)
 user_cron.write()
