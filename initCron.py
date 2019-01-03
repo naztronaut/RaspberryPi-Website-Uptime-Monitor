8f89370b4d27f057f79c11cf15c71588bba6e436
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-01-02 22:02:47 -0500

    updated cron tasks

--- initCron.py
+++ initCron.py
@@ -15,11 +15,11 @@ cronList = [
 def cronCheckSites():
     for item in cronList:
         job = user_cron.new(command='cd /home/pi/uptime; /home/pi/uptime/venv/bin/python3 /home/pi/uptime/' +
-                            item.scriptName + '.py', comment='checksites')
-        job.minute.every(item.frequency)
+                            item['scriptName'] + '.py', comment='checksites')
+        job.minute.every(item['frequency'])
         user_cron.write()
-        db.addCron(item.scriptName, item.frequency, item.cronScript, 1)
-        print('Successfully added job titled ' + item.name)
+        db.addCron(item['scriptName'], item['frequency'], item['cronScript'], 1)
+        print('Successfully added job titled ' + item['name'])
 
 
 cronCheckSites()
