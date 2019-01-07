e79f1600d9c07acf6a540ab1d4a71b6475cac674
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-01-07 00:56:43 -0500

    testing email script

--- outageEmail.py
+++ outageEmail.py
@@ -8,7 +8,7 @@ def outage(sites, downCount):
     msg = MIMEMultipart("alternative")
 
     msg['Subject'] = 'Website outage detected!'
-    msg['From'] = 'epuptime'
+    msg['From'] = 'EP Uptime Monitor'
     msg['To'] = 'njoker555@gmail.com'
 
     username = cred.EMAIL_CONFIG['username']
@@ -16,9 +16,9 @@ def outage(sites, downCount):
     port = 465
 
     html = """
-        The following websites have been detected to be down, please check immediately: <br />
+        The following websites have been detected to be down, please check the %s website(s) immediately: <br />
         %s
-    """ % (sites)
+    """ % (downCount, sites)
 
     emailMsg = MIMEText(html, "html")
 
@@ -27,44 +27,7 @@ def outage(sites, downCount):
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
             server.login(username, password)
-            server.sendmail('epuptime@gmail.com','njoker555@gmail.com',msg.as_string())
+            server.sendmail('epuptime@gmail.com', 'njoker555@gmail.com', msg.as_string())
 
 
 outage(['easyprogramming', 'postsession'], 3)
-
-# import smtplib, ssl
-# import emails.emailcred as cred
-#
-# from email.mime.text import MIMEText
-# from email.mime.multipart import MIMEMultipart
-#
-# # def outage(sites, downCount):
-# message = MIMEMultipart("alternative")
-#
-# message['Subject'] = '%s Detected Website Outage'
-# message['From'] = 'epuptime'
-# message['To'] = 'njoker555@gmail.com'
-#
-# username = cred.EMAIL_CONFIG['username']
-# password = cred.EMAIL_CONFIG['password']
-# print(username)
-# print(password)
-# port = 465
-#
-# htmlEmail = """
-#     The following websites have been detected to be down, please check immediately: <br />
-#     %s
-# """
-#
-# emailMsg = MIMEText(htmlEmail, "html")
-# message.attach(emailMsg)
-#
-# context = ssl.create_default_context()
-#
-# with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-#     server.login(username, password)
-#     server.sendmail('epuptime@gmail.com', 'njoker555@gmail.com', message)
-
-
-# outage('easyprogramming', 3)
-
