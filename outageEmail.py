1fb085d0a740e8745b61a07b4837218060518450
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-01-07 00:48:41 -0500

    testing email script

--- outageEmail.py
+++ outageEmail.py
@@ -4,30 +4,33 @@ import emails.emailcred as cred
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-msg = MIMEMultipart("alternative")
+def outage(sites, downCount):
+    msg = MIMEMultipart("alternative")
 
-msg['Subject'] = 'Testing from python'
-msg ['From'] = 'epuptime'
-msg['To'] = 'njoker555@gmail.com'
+    msg['Subject'] = 'Testing from python'
+    msg ['From'] = 'epuptime'
+    msg['To'] = 'njoker555@gmail.com'
 
-username = cred.EMAIL_CONFIG['username']
-password = cred.EMAIL_CONFIG['password']
-port = 465
+    username = cred.EMAIL_CONFIG['username']
+    password = cred.EMAIL_CONFIG['password']
+    port = 465
 
-html = """
-    The following websites have been detected to be down, please check immediately: <br />
-    %s
-""" % ('easyprogramming')
+    html = """
+        The following websites have been detected to be down, please check immediately: <br />
+        %s
+    """ % (sites)
 
-emailMsg = MIMEText(html, "html")
+    emailMsg = MIMEText(html, "html")
 
-msg.attach(emailMsg)
+    msg.attach(emailMsg)
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        server.login(username, password)
-        server.sendmail('epuptime@gmail.com','njoker555@gmail.com',msg.as_string())
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+            server.login(username, password)
+            server.sendmail('epuptime@gmail.com','njoker555@gmail.com',msg.as_string())
 
+
+outage('easyprogramming', 3)
 
 # import smtplib, ssl
 # import emails.emailcred as cred
