25297db6b4712f7c858d9aeca618a108c31a6a19
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-01-07 20:22:59 -0500

    added cronOutage.py

--- cronOutage.py
+++ cronOutage.py
@@ -1,7 +1,15 @@
 import reporting_db as rdb
-
+import outageEmail as oe
 
 data = rdb.getDownTimeCountsGreaterThanThree()
 
+downSites = []
+count = 0
+
 for item in data:
     print(item)
+    if item['downCount'] >= 3:
+        count += 1
+        downSites.append(item['site'])
+
+oe.outage(downSites, count)
