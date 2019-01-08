1799a0cc7ae02cacbd6b37b6ab9e5112377e64e7
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-01-08 01:04:38 -0500

    fixed bug in cronOutage"

--- cronOutage.py
+++ cronOutage.py
@@ -5,12 +5,13 @@ data = rdb.getDownTimeCountsGreaterThanThree()
 
 downSites = []
 count = 0
+print(len(data))
+if len(data) > 0:
+    for item in data:
+        # print(item)
+        # Only send an email if check has failed more than 3 times and only every 3 checks to minimize emails
+        if item['downCount'] >= 3 and item['downCount'] % 3 == 0:
+            count += 1
+            downSites.append(item)
 
-for item in data:
-    # print(item)
-    # Only send an email if check has failed more than 3 times and only every 3 checks to minimize emails
-    if item['downCount'] >= 3 and item['downCount'] % 3 == 0:
-        count += 1
-        downSites.append(item)
-
-oe.outage(downSites, count)
+    oe.outage(downSites, count)
