85f43485741d89317c23a0bff1bab509fd452fe2
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-01-11 01:35:14 -0500

    fixed tabbing in upService.py

--- upService.py
+++ upService.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def homePage():
-	return "There's nothing here. Find your own way!"
+    return "There's nothing here. Find your own way!"
 
 
 # Manually updates all statuses and updates the database
@@ -23,29 +23,29 @@ def getJson():
 
 @app.route('/getCurrentStatus', methods=['GET'])
 def getCurrentStatus():
-	page = request.args.get('page',default=1,type=int)
-	limit = request.args.get('limit',default=10,type=int)
-	data = db.getCurrentStatus(page, limit)
-	return jsonify(data)
+    page = request.args.get('page',default=1,type=int)
+    limit = request.args.get('limit',default=10,type=int)
+    data = db.getCurrentStatus(page, limit)
+    return jsonify(data)
 
 
 @app.route('/getActivity', methods=['GET'])
 def getActivity():
-	page = request.args.get('page',default=1,type=int)
-	limit = request.args.get('limit',default=25,type=int)
-	data = db.getActivity(page, limit)
-	return jsonify(data)
+    page = request.args.get('page',default=1,type=int)
+    limit = request.args.get('limit',default=25,type=int)
+    data = db.getActivity(page, limit)
+    return jsonify(data)
 
 @app.route('/getOutages', methods=['GET'])
 def getOutages():
-	page = request.args.get('page',default=1,type=int)
-	limit = request.args.get('limit',default=25,type=int)
-	data = db.getOutages(page, limit)
-	return jsonify(data)
+    page = request.args.get('page',default=1,type=int)
+    limit = request.args.get('limit',default=25,type=int)
+    data = db.getOutages(page, limit)
+    return jsonify(data)
 
 @app.route('/getDowntimeCounts', methods=['GET'])
 def getDowntimeCounts():
-	page = request.args.get('page',default=1,type=int)
-	limit = request.args.get('limit',default=25,type=int)
-	data = db.getOutages(page, limit)
-	return jsonify(data)
+    page = request.args.get('page',default=1,type=int)
+    limit = request.args.get('limit',default=25,type=int)
+    data = db.getOutages(page, limit)
+    return jsonify(data)
