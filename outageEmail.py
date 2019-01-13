b41961210dcd83261e16624f63a2e9c774da8708
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-01-13 18:32:41 -0500

    save to database on each notification - testing

--- outageEmail.py
+++ outageEmail.py
@@ -4,6 +4,8 @@ import config.config as cred
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+import db
+
 
 def outage(sites, downCount):
 
@@ -36,11 +38,11 @@ def outage(sites, downCount):
     msg.attach(emailMsg)
 
     # Add to database - to fix later, currently cannot hold html
-    # db.addNotification(html.as_string())
+    db.addNotification(str(sites))
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtpServer, port, context=context) as server:
             server.login(username, password)
             server.sendmail(sender, recipient, msg.as_string())
 
-# outage(['easyprogramming', 'postsession'], 3)
+outage(['easyprogramming', 'postsession'], 3)
