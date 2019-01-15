c13fc7060dfa1c7e202ea4087af3eabddae1fb5d
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-01-14 22:51:47 -0500

    fixed db notifications storage

--- outageEmail.py
+++ outageEmail.py
@@ -50,8 +50,8 @@ def outage(sites, downCount):
         with smtplib.SMTP_SSL(smtpServer, port, context=context) as server:
                 server.login(username, password)
                 server.sendmail(sender, recipient, msg.as_string())
-                db.addNotification('sites not recorded', 'success')
+                db.addNotification(str(downSites), 'success')
                 print('Email Sent successfully')
     except:
         print('Error sending email')
-        db.addNotification('sites not recorded', 'fail')
+        db.addNotification(str(downSites), 'fail')
