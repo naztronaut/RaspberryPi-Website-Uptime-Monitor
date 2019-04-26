4ebcf4279f999b9f5486cb76075f71993d6f496f
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-04-25 23:20:05 -0400

    testing uptime

--- uptime.py
+++ uptime.py
@@ -4,6 +4,7 @@ import json
 import db
 import reporting_db as rdb
 import config.config as config
+from flask import jsonify
 
 green = config.LED_PINS['green']
 yellow = config.LED_PINS['yellow']
@@ -21,9 +22,9 @@ def sites():
     totalSites = 0
     # Read list of sites in sites.txt - one site per line
     # make sure the up.json file exists with the property "site"
-    json_object = json.load(rdb.getSites(1, 100))
-    print(json_object)
-    return json_object
+    json_object = jsonify(rdb.getSites(1, 100))
+    print(json.load(json_object))
+    return json.load(json_object)
     # with open("sites.txt") as f:
     #     for line in f:
     #         totalSites = totalSites + 1
