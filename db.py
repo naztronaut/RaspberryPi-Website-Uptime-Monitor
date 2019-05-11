85c2559b8e5a55efc58f5cce31e38e695324ca36
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-05-11 04:05:09 -0400

    opened and closed cursor in each method

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ db = MySQLdb.connect(config.DATABASE_CONFIG['host'], config.DATABASE_CONFIG['dbu
                      config.DATABASE_CONFIG['dbname'],
                      cursorclass=MySQLdb.cursors.DictCursor)
 
-cursor = db.cursor()
+# cursor = db.cursor()
 
 
 ###########################################################################################
@@ -18,18 +18,23 @@ cursor = db.cursor()
 
 # Add all activities to `activity` table for historical purposes and reporting
 def addActivity(statustype, sites):
+    cursor = db.cursor()
     cursor.execute("""INSERT INTO activity (activityType, sitesAffected) VALUES (%s, %s)""", (statustype, sites))
     db.commit()
+    cursor.close()
 
 
 # Get current status of all sites
 def currentStatus(site, status):
+    cursor = db.cursor()
     cursor.execute("""INSERT INTO currentStatus (site, status) VALUES (%s,%s)
                         ON DUPLICATE KEY UPDATE status = %s""", (site, status, status))
+    cursor.close()
 
 
 # Add site into sites table -  newer than currentStatus
 def addSite(name, url, status, active, email, visible):
+    cursor = db.cursor()
     cursor.execute("""INSERT INTO sites (siteName, url, status, active, email, visible) 
                             VALUES (%s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE siteName = %s, url = %s, status = %s, 
                             active = %s, email = %s, visible = %s""",
@@ -37,25 +42,30 @@ def addSite(name, url, status, active, email, visible):
     db.commit()
     cursor.execute("""SELECT * FROM sites where url = %s""", [url])
     data = cursor.fetchone()
+    cursor.close()
     return data
 
 
 # Update sites and return site after edit
 def updateSite(id, name, url, active, email, visible):
+    cursor = db.cursor()
     cursor.execute("""UPDATE sites SET siteName = %s, url = %s,
                             active = %s, email = %s, visible = %s WHERE id = %s""",
                    (name, url, active, email, visible, id))
     db.commit()
     cursor.execute("""SELECT * FROM sites where id = %s""", [id])
     data = cursor.fetchone()
+    cursor.close()
     return data
 
 
 # Update status of site if it's down
 def updateSiteStatus(id, status):
+    cursor = db.cursor()
     cursor.execute("""UPDATE sites SET status = %s WHERE id = %s""",
                    (status, id))
     db.commit()
+    cursor.close()
 
 
 ###########################################################################################
@@ -73,16 +83,20 @@ def updateSiteStatus(id, status):
 
 # Insert sites into `outages` as an array
 def insertSites(sites, numSites):
+    cursor = db.cursor()
     cursor.execute("""INSERT INTO outages (sitesAffected, numberOfSites) values (%s,%s)""", (sites, numSites))
     db.commit()
+    cursor.close()
 
 
 # Insert sites that are offline to `downtimeCounts` - if they already exist, increase the downCount by 1
 # Should trigger an email after 3
 def insertDownSite(site):
+    cursor = db.cursor()
     cursor.execute("""INSERT INTO downtimeCounts (site, downCount) VALUES(%s, 1)
                         ON DUPLICATE KEY UPDATE downCount=downCount+1""", [site])
     db.commit()
+    cursor.close()
 
 
 # first checks to see if site exists in the `downtimeCounts` table -
@@ -90,12 +104,14 @@ def insertDownSite(site):
 # if entry exists, set the downCount to 0
 # table should be truncated overnight (need to implement archive table)
 def checkSite(site):
+    cursor = db.cursor()
     data = cursor.execute("""SELECT 1 FROM downtimeCounts where site = %s""", [site])
     # print(data)
     if data >= 1:
         cursor.execute("""UPDATE downtimeCounts set downCount = 0 where site = %s""", [site])
         # print(site + " is back up!")
     db.commit()
+    cursor.close()
 
 
 ###########################################################################################
@@ -112,14 +128,18 @@ def checkSite(site):
 ###########################################################################################
 # LED Table
 def changeLedStatus(color, status):
+    cursor = db.cursor()
     cursor.execute("""UPDATE ledStatus set status = %s where pin = %s""", (status, color))
     db.commit()
+    cursor.close()
 
 
 # Override led
 def overrideLedActive(color, active):
+    cursor = db.cursor()
     cursor.execute("""UPDATE ledStatus set active = %s where color = %s""", (active, color))
     db.commit()
+    cursor.close()
 
 ###########################################################################################
 #                                                                                         #
@@ -136,21 +156,25 @@ def overrideLedActive(color, active):
 
 # Add new cronjob to database
 def addCron(comment, cronName, cronVal, cronScript, enabled):
-        cursor.execute("""INSERT INTO cronSettings (comment, cronName, cronVal, cronScript, enabled) 
-                        VALUES (%s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE cronName = %s, cronVal = %s, 
-                        enabled = %s""", (comment, cronName, cronVal, cronScript, enabled, cronName, cronVal, enabled))
-        db.commit()
+    cursor = db.cursor()
+    cursor.execute("""INSERT INTO cronSettings (comment, cronName, cronVal, cronScript, enabled) 
+                    VALUES (%s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE cronName = %s, cronVal = %s, 
+                    enabled = %s""", (comment, cronName, cronVal, cronScript, enabled, cronName, cronVal, enabled))
+    db.commit()
+    cursor.close()
 
 
 
 # Find and update cron job in database
 def updateCron(comment, cronName, cronVal, enabled):
-        cursor.execute("""UPDATE cronSettings set cronName = %s, cronVal = %s, enabled = %s WHERE
-                        comment = %s""", (cronName, cronVal, enabled, comment))
-        db.commit()
-        cursor.execute("""SELECT * FROM cronSettings where comment = %s""", [comment])
-        data = cursor.fetchone()
-        return data
+    cursor = db.cursor()
+    cursor.execute("""UPDATE cronSettings set cronName = %s, cronVal = %s, enabled = %s WHERE
+                    comment = %s""", (cronName, cronVal, enabled, comment))
+    db.commit()
+    cursor.execute("""SELECT * FROM cronSettings where comment = %s""", [comment])
+    data = cursor.fetchone()
+    cursor.close()
+    return data
 
 ###########################################################################################
 #                                                                                         #
@@ -166,8 +190,10 @@ def updateCron(comment, cronName, cronVal, enabled):
 ###########################################################################################
 
 def addNotification(content, status):
+    cursor = db.cursor()
     cursor.execute("""INSERT INTO notifications (content, status) values (%s,%s)""", (content, status))
     db.commit()
+    cursor.close()
 
 
 ###########################################################################################
@@ -186,12 +212,14 @@ def addNotification(content, status):
 # Archive records from downtime counts when records are more than 3 days old
 
 def archiveDowntimeCounts():
+    cursor = db.cursor()
     cursor.execute("""INSERT INTO archive_downtimeCounts (created_at, site, downCount) 
                     (SELECT created_at,site,downCount FROM downtimeCounts where created_at < (CURDATE() - 3))""")
     db.commit()
 
     cursor.execute("""DELETE FROM downtimeCounts where created_at < (CURDATE() - 3))""")
     db.commit()
+    cursor.close()
 
 
 ###########################################################################################
